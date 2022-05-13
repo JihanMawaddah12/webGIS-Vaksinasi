a6c242aff82a461e3ff8c76c39c76ce9fb68c866
riski86-oss <riskirahmat155@gmail.com>
2022-05-13 21:11:57 +0700

    update

--- resources/views/dataUser.blade.php
+++ resources/views/dataUser.blade.php
@@ -25,7 +25,9 @@ http://www.tooplate.com/view/2091-ziggy
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('storage/css/adminlte.min.css') }}" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
     </script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css"
@@ -96,7 +98,7 @@ http://www.tooplate.com/view/2091-ziggy
         </a>
     </section>
 
-    <section class="second-section" style=" background-color: #EDE6DB">
+    <section class="second-section text-center" style=" background-color: #EDE6DB">
         <h1>Vaksinasi COVID-19 Kota Banda Aceh</h1><br>
         <div class="container">
             <div class="row">
@@ -850,8 +852,6 @@ crossorigin="anonymous" referrerpolicy="no-referrer"></script>
         position: 'bottomright'
     });
 
-    //pemanggilan legend
-   
     var markersLayer = new L.LayerGroup(); //layer contain searched elements
         map.addLayer(markersLayer);
         var controlSearch = new L.Control.Search({
