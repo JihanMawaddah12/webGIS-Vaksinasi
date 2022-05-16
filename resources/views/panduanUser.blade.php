20f7f3013bcb5d063ef57f0cce502c2df2285f88
riski86-oss <riskirahmat155@gmail.com>
2022-05-16 15:08:29 +0700

    update

--- resources/views/panduanUser.blade.php
+++ resources/views/panduanUser.blade.php
@@ -115,29 +115,22 @@ http://www.tooplate.com/view/2091-ziggy
     <section class="second-section" style=" background-color: #EDE6DB">
         <div class="container">
             <div class="card p-4">
-                <div class="col-md-8">
-                    <h3><b>Cara Melakukan Pendaftaran Vaksinasi
-                </div>
-                <div>
-                    <span class="bg-success me-2 text-white rounded-circle px-2" style="width: fit-content">
-                        1
-                    </span> Tekan tombol "Daftar Vaksinasi"
-                </div>
-                <div class="text-center"><img width="700" src="{{ asset('storage/img/daftar.png') }}" alt="">
+                <h3><b>Cara Melakukan Pendaftaran Vaksinasi</b></h3>
+                <div class="mt-2">
+                    <div class="mb-2">
+                        <span class="bg-success me-2 me-2 text-white rounded-circle px-2" style="width: fit-content">
+                            1
+                        </span> Tekan tombol "Daftar Vaksinasi"
+                    </div>
+                    <div class="text-center"><img width="700" src="{{ asset('storage/img/daftar.png') }}" alt="">
+                    </div>
                 </div>
 
-                <div class="row">
-                    <div class="col">
-                        <div class="row mb-2">
-                            <div class="col-md-1">
-                                <span class="bg-success text-white rounded-circle px-2" style="width: fit-content">
-                                    2
-                                </span>
-                            </div>
-                            <div class="col-md-8">
-                                Isi data diri Anda, lalu tekan tombol daftar
-                            </div>
-                        </div>
+                <div class="mt-2">
+                    <div class="mb-2">
+                        <span class="bg-success me-2 text-white rounded-circle px-2" style="width: fit-content">
+                            2
+                        </span> Isi data diri Anda, lalu tekan tombol daftar
                     </div>
                     <div class="text-center"><img width="700" src="{{ asset('storage/img/IsiData.png') }}" alt="">
                     </div>
@@ -147,62 +140,42 @@ http://www.tooplate.com/view/2091-ziggy
             </div>
         </div>
 
-        <div class="container">
+        <div class="mt-3 container">
             <div class="card p-4">
                 <div class="col">
-                    <h3><b>Jika Anda Pihak Gerai Vaksin/PUSKESMAS/Rumah Sakit
+                    <h3><b>Jika Anda Pihak Gerai Vaksin/PUSKESMAS/Rumah Sakit</b></h3>
                 </div>
-                <div class="row">
-                    <div class="col">
-                        <div class="row mb-2">
-                            <div class="col-md-1">
-                                <span class="bg-success text-white rounded-circle px-2" style="width: fit-content">
-                                    1
-                                </span>
-                            </div>
-                            <div class="col-md-8">
-                                Login menggunakan Akun yang sudah diberikan
-                            </div>
-                        </div>
+                <div class="mt-2">
+                    <div class="mb-2">
+                        <span class="bg-success me-2 text-white rounded-circle px-2" style="width: fit-content">
+                            1
+                        </span>Login menggunakan Akun yang sudah diberikan
                     </div>
+
                     <div class="text-center"><img width="700" src="{{ asset('storage/img/LoginRS.png') }}" alt="">
                     </div>
-
                 </div>
 
-                <div class="row">
-                    <div class="col">
-                        <div class="row mb-2">
-                            <div class="col-md-1">
-                                <span class="bg-success text-white rounded-circle px-2" style="width: fit-content">
-                                    2
-                                </span>
-                            </div>
-                            <div class="col-md-10">
-                                Tekan tombol ceklis jika masyarakat berhasil melakukan vaksinasi dan tekan tombol hapus
-                                jika masyarakat batal melakukan vaksinasi
-                            </div>
-                        </div>
+
+                <div class="mt-2">
+                    <div class="mb-2">
+                        <span class="bg-success me-2 text-white rounded-circle px-2" style="width: fit-content">
+                            2
+                        </span> Tekan tombol ceklis jika masyarakat berhasil melakukan vaksinasi dan tekan tombol hapus
+                        jika masyarakat batal melakukan vaksinasi
                     </div>
                     <div class="text-center"><img width="700" src="{{ asset('storage/img/DataDaftar.png') }}"
                             alt=""></div>
-
                 </div>
 
-                <div class="row">
-                    <div class="col">
-                        <div class="row mb-2">
-                            <div class="col-md-1">
-                                <span class="bg-success text-white rounded-circle px-2" style="width: fit-content">
-                                    3
-                                </span>
-                            </div>
-                            <div class="col-md-10">
-                                Menu untuk melihat data yang sudah terverifikasi, jika data sudah
-                                tidak dibutuhkan tekan tombol hapus
-                            </div>
-                        </div>
+                <div class="mt-2">
+                    <div class="mb-2">
+                        <span class="bg-success me-2 text-white rounded-circle px-2" style="width: fit-content">
+                            3
+                        </span>Menu untuk melihat data yang sudah terverifikasi, jika data sudah
+                        tidak dibutuhkan tekan tombol hapus
                     </div>
+
                     <div class="text-center"><img width="700" src="{{ asset('storage/img/verifikasi.png') }}"
                             alt=""></div>
 
@@ -211,83 +184,61 @@ http://www.tooplate.com/view/2091-ziggy
             </div>
         </div>
 
-        <div class="container">
+        <div class="mt-3 container">
             <div class="card p-4">
                 <div class="col">
-                    <h3><b>Jika Anda Pihak Dinas Kesehatan
+                    <h3><b>Jika Anda Pihak Dinas Kesehatan</b></h3>
                 </div>
-                <div class="row">
-                    <div class="col">
-                        <div class="row mb-2">
-                            <div class="col-md-1">
-                                <span class="bg-success text-white rounded-circle px-2" style="width: fit-content">
-                                    1
-                                </span>
-                            </div>
-                            <div class="col-md-8">
-                                Login menggunakan Akun yang sudah diberikan
-                            </div>
-                        </div>
-                        <div class="text-center"><img width="700" src="{{ asset('storage/img/loginAdmin.png') }}"
-                                alt=""></div>
+                <div class="mt-2">
+                    <div class="mb-2">
+                        <span class="bg-success me-2 text-white rounded-circle px-2" style="width: fit-content">
+                            1
+                        </span> Login menggunakan Akun yang sudah diberikan
                     </div>
 
+                    <div class="text-center"><img width="700" src="{{ asset('storage/img/loginAdmin.png') }}"
+                            alt=""></div>
+
                 </div>
 
-                <div class="row">
-                    <div class="col">
-                        <div class="row mb-2">
-                            <div class="col-md-1">
-                                <span class="bg-success text-white rounded-circle px-2" style="width: fit-content">
-                                    2
-                                </span>
-                            </div>
-                            <div class="col-md-10">
-                                Menu dashboard akan menampilkan informasi capaian vaksinasi
-                            </div>
-                        </div>
-                        <div class="text-center"><img width="700" src="{{ asset('storage/img/dashboard.png') }}"
-                                alt=""></div>
+                <div class="mt-2">
+                    <div class="mb-2">
+                        <span class="bg-success me-2 text-white rounded-circle px-2" style="width: fit-content">
+                            2
+                        </span> Menu dashboard akan menampilkan informasi capaian vaksinasi
                     </div>
 
+                    <div class="text-center"><img width="700" src="{{ asset('storage/img/dashboard.png') }}"
+                            alt=""></div>
+
                 </div>
 
-                <div class="row">
-                    <div class="col">
-                        <div class="row mb-2">
-                            <div class="col-md-1">
-                                <span class="bg-success text-white rounded-circle px-2" style="width: fit-content">
-                                    3
-                                </span>
-                            </div>
-                            <div class="col-md-10">
-                                Menu Maps menampilkan beberapa informasi dalam bentuk peta seperti sebaran lokasi, desa,
-                                kecamatan dan rute
-                            </div>
-                        </div>
-                        <div class="text-center"><img width="700" src="{{ asset('storage/img/lokasi.png') }}"
-                                alt=""></div>
+                <div class="mt-2">
+                    <div class="mb-2">
+                        <span class="bg-success me-2 text-white rounded-circle px-2" style="width: fit-content">
+                            3
+                        </span> Menu Maps menampilkan beberapa informasi dalam bentuk peta seperti sebaran lokasi, desa,
+                        kecamatan dan rute
                     </div>
 
+                    <div class="text-center"><img width="700" src="{{ asset('storage/img/lokasi.png') }}" alt="">
+                    </div>
                 </div>
 
-                <div class="row">
-                    <div class="col">
-                        <div class="row mb-2">
-                            <div class="col-md-1">
-                                <span class="bg-success text-white rounded-circle px-2" style="width: fit-content">
-                                    4
-                                </span>
-                            </div>
-                            <div class="col-md-10">
-                                Menu Data menampilkan beberapa informasi dalam bentuk tabel seperti data vaksinasi, data
-                                lokasi,
-                                data desa, data kecamatan dan data user. Pada halaman ini Admin juga dapat melakukan
-                                input data atau edit data
-                            </div>
-                        </div>
-                        <div class="text-center"><img width="700" src="{{ asset('storage/img/data.png') }}"
-                                alt=""></div>
+
+
+                <div class="mt-2">
+                    <div class="mb-2">
+                        <span class="bg-success me-2 text-white rounded-circle px-2" style="width: fit-content">
+                            4
+                        </span>
+                        Menu Data menampilkan beberapa informasi dalam bentuk tabel seperti data vaksinasi, data
+                        lokasi,
+                        data desa, data kecamatan dan data user. Pada halaman ini Admin juga dapat melakukan
+                        input data atau edit data
+                    </div>
+                   
+                    <div class="text-center"><img width="700" src="{{ asset('storage/img/data.png') }}" alt="">
                     </div>
 
                 </div>
