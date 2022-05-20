0ef4f74808336bdd0b000515734b2ffa46dfc38b
jihan <jihan18@mhs.unsyiah.ac.id>
2022-05-20 15:46:35 +0700

    update

--- resources/views/tambah_data.blade.php
+++ resources/views/tambah_data.blade.php
@@ -8,6 +8,10 @@
                 <div class="row">
                     <div class="col-md-6">
                         <div class="form-group">
+                            <label>Tanggal</label>
+                            <input name="tanggal" type="date" class="form-control" required>
+                        </div>
+                        <div class="form-group">
                             <label>Kecamatan</label>
                             <select class="form-select" name="tematik_id" required>
                                 <option value="">Pilih Kecamatan</option>
@@ -16,16 +20,7 @@
                                 @endforeach
                             </select>
                         </div>
-                        <div class="form-group">
-                            <label>Dosis</label>
-                            <select class="form-select" name="kelompok" required>
-                                <option value="">Pilih Dosis</option>
-                                <option value="Target">Target</option>
-                                <option value="Dosis 1">Dosis 1</option>
-                                <option value="Dosis 2">Dosis 2</option>
-                                <option value="Dosis 3">Dosis 3</option>
-                            </select>
-                        </div>
+
                         <div class="form-group">
                             <label>Nakes</label>
                             <input name="nakes" type="number" class="form-control" required>
@@ -35,11 +30,22 @@
                             <input name="petugas_publik" type="number" class="form-control" required>
                         </div>
                         <div class="form-group">
-                            <label>Tanggal</label>
-                            <input name="tanggal" type="date" class="form-control" required>
+                            <label>Lansia</label>
+                            <input name="lansia" type="number" class="form-control" required>
                         </div>
+
                     </div>
                     <div class="col-md-6">
+                        <div class="form-group">
+                            <label>Kelompok</label>
+                            <select class="form-select" name="kelompok" required>
+                                <option value="">Pilih Kelompok</option>
+                                <option value="Target">Target</option>
+                                <option value="Dosis 1">Dosis 1</option>
+                                <option value="Dosis 2">Dosis 2</option>
+                                <option value="Dosis 3">Dosis 3</option>
+                            </select>
+                        </div>
                         <div class="form-group">
                             <label>Desa</label>
                             <select class="form-select" name="desa_id" required>
@@ -49,10 +55,7 @@
                                 @endforeach
                             </select>
                         </div>
-                        <div class="form-group">
-                            <label>Lansia</label>
-                            <input name="lansia" type="number" class="form-control" required>
-                        </div>
+
                         <div class="form-group">
                             <label>Masyarakat Umum</label>
                             <input name="masyarakat_umum" type="number" class="form-control" required>
